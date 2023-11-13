091e3f1b3d1765b73a0e7827c8a896a5741ee8c3
RoundedMecha <150624296+RoundedMecha@users.noreply.github.com>
2023-11-13 17:06:55 +0000

    Update EtrianOdyssey2HDSaveImporter.sh

--- EtrianOdyssey2HDSaveImporter.sh
+++ EtrianOdyssey2HDSaveImporter.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+now=$(date)
+
 if test -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" &&  test -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/"; then
     echo "Etrian 1 & 2 Found Copying Save Data Over"
     cp -r "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA"
+else if [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" ] || [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/" ] ; then
+    echo "$now: Directory Missing" >> Error.txt
 else
-    echo "No Etrian HD Save File Found"
+    echo "$now: Generic Error" >> Error.txt
 fi
