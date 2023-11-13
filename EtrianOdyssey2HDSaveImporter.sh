a798f842c6ca746d66bf8bd080d85ef33f06ebb0
RoundedMecha <150624296+RoundedMecha@users.noreply.github.com>
2023-11-13 17:22:30 +0000

    Update EtrianOdyssey2HDSaveImporter.sh
    Renamed Text File

--- EtrianOdyssey2HDSaveImporter.sh
+++ EtrianOdyssey2HDSaveImporter.sh
@@ -3,10 +3,10 @@
 now=$(date)
 
 if test -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" &&  test -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/"; then
-    echo "$now Etrian 1 & 2 Found Copying Save Data Over" >> Error.txt
+    echo "$now Etrian 1 & 2 Found Copying Save Data Over" >> Debug.txt
     cp -r "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA"
 elif [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" ] || [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/" ] ; then
-    echo "$now: Directory Missing" >> Error.txt
+    echo "$now: Directory Missing" >> Debug.txt
 else
-    echo "$now: Generic Error" >> Error.txt
+    echo "$now: Generic Error" >> Debug.txt
 fi
