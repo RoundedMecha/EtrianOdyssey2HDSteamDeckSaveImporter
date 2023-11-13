b3232931e1d8d621b47393bcf88ba983e4cd5e9a
RoundedMecha <150624296+RoundedMecha@users.noreply.github.com>
2023-11-13 17:40:01 +0000

    Update EtrianOdyssey2HDSaveImporter.sh
    Added Check For Successful Copy

--- EtrianOdyssey2HDSaveImporter.sh
+++ EtrianOdyssey2HDSaveImporter.sh
@@ -4,7 +4,7 @@ now=$(date)
 
 if test -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" &&  test -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/"; then
     echo "$now Etrian 1 & 2 Found Copying Save Data Over" >> Debug.txt
-    cp -r "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA"
+    cp -r "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA" 2>>Debug.txt && echo "Pass" || echo "failed"
 elif [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" ] || [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/" ] ; then
     echo "$now: Directory Missing" >> Debug.txt
 else
