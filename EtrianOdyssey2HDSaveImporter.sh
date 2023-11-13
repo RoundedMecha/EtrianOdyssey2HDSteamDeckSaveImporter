c12b983112b0123751b5214a3f553ccc89a06058
RoundedMecha <150624296+RoundedMecha@users.noreply.github.com>
2023-11-13 17:18:26 +0000

    Update EtrianOdyssey2HDSaveImporter.sh
    - Fix Syntax Error
    - Added Sucess Message to File

--- EtrianOdyssey2HDSaveImporter.sh
+++ EtrianOdyssey2HDSaveImporter.sh
@@ -3,9 +3,9 @@
 now=$(date)
 
 if test -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" &&  test -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/"; then
-    echo "Etrian 1 & 2 Found Copying Save Data Over"
+    echo "$now Etrian 1 & 2 Found Copying Save Data Over" >> Error.txt
     cp -r "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA"
-else if [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" ] || [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/" ] ; then
+elif [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868180/pfx/drive_c/users/steamuser/Application Data/SEGA/EOHD" ] || [ ! -d "/home/deck/.local/share/Steam/steamapps/compatdata/1868170/pfx/drive_c/users/steamuser/Application Data/SEGA/EO2HD/" ] ; then
     echo "$now: Directory Missing" >> Error.txt
 else
     echo "$now: Generic Error" >> Error.txt
